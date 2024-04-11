09331d3a8a7639324e78973a6a3be8151297710a
Karen Hanna <karen@laveryhanna.com>
2024-04-11 16:17:55 +0100

    updated unit tests

---
+++ src/SFA.DAS.ProviderFeedback.ApplicationUnitTests/Commands/GetProviderFeedbackHandlerTest.cs
@@ -0,0 +1,114 @@
+﻿using AutoFixture;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.ProviderFeedback.Application.Queries.GetProviderFeedback;
+using SFA.DAS.ProviderFeedback.Domain.ApiResponse;
+using SFA.DAS.ProviderFeedback.Domain.GetProviderFeedback;
+using SFA.DAS.ProviderFeedback.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ProviderFeedback.Application.UnitTests.Commands
+{
+    [TestFixture]
+    public class GetProviderFeedbackHandlerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task GetProviderFeedbackHandler() 
+        {
+            // Arrange
+            var mockApiClient = new Mock<IApiClient>();
+            var command = new GetProviderFeedbackQuery();
+            var handler = new GetProviderFeedbackQueryHandler(mockApiClient.Object);
+
+            var request = new GetProviderFeedbackRequest(123456789);
+
+            var expectedResponse = new GetProviderFeedbackResponse
+            {
+                ProviderFeedback = new Feedback.ProviderFeedbackModel 
+                {
+                    ProviderId = request.ProviderId,
+                    EmployerFeedback = new Feedback.EmployerFeedback() 
+                    { 
+                        TotalEmployerResponses = 2,
+                        TotalFeedbackRating = 4,
+                        FeedbackAttributes = new List<Feedback.EmployerFeedbackAttributeDetail>()
+                        { 
+                            new Feedback.EmployerFeedbackAttributeDetail
+                            { 
+                                AttributeName = "Attribute One",
+                                Rating = 5,
+                                Strength = 2,
+                                TotalVotes = 2,
+                                Weakness = 0,
+                            },
+                            new Feedback.EmployerFeedbackAttributeDetail
+                            {
+                                AttributeName = "Attribute Two",
+                                Rating = 3,
+                                Strength = 2,
+                                TotalVotes = 2,
+                                Weakness = 0,
+                            }
+                        }
+                    },
+                    ApprenticeFeedback = new Feedback.ApprenticeFeedback 
+                    {
+                        TotalApprenticeResponses = 5,
+                        TotalFeedbackRating = 3,
+                        FeedbackAttributes = new List<Feedback.ApprenticeFeedbackAttributeDetail> 
+                        {
+                            new Feedback.ApprenticeFeedbackAttributeDetail
+                            {
+                                Name = "Apprentice Attribute 1",
+                                Agree = 4,
+                                Disagree =1,
+                                Category = "Category A",
+                                Rating = 3,
+                                TotalVotes = 5
+                            },
+                            new Feedback.ApprenticeFeedbackAttributeDetail
+                            {
+                                Name = "Apprentice Attribute 2",
+                                Agree = 3,
+                                Disagree =2,
+                                Category = "Category B",
+                                Rating = 3,
+                                TotalVotes = 5
+                            },
+
+                        }
+                    }
+                }
+            };
+
+            mockApiClient
+                .Setup(api => api.Get<GetProviderFeedbackResponse>(It.IsAny<GetProviderFeedbackRequest>()))
+                .ReturnsAsync(new ApiResponse<GetProviderFeedbackResponse>(expectedResponse, HttpStatusCode.OK, string.Empty));
+
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+
+            mockApiClient.Verify(x => x.Get<GetProviderFeedbackResponse>(It.IsAny<GetProviderFeedbackRequest>()), Times.Once);
+            Assert.AreEqual(expectedResponse.ProviderFeedback.ApprenticeFeedback.TotalApprenticeResponses, result.ApprenticeFeedback.TotalApprenticeResponses);
+            Assert.AreEqual(expectedResponse.ProviderFeedback.ApprenticeFeedback.TotalFeedbackRating, result.ApprenticeFeedback.TotalFeedbackRating);
+            Assert.AreEqual(expectedResponse.ProviderFeedback.ApprenticeFeedback.FeedbackAttributes.Count, result.ApprenticeFeedback.FeedbackAttributes.Count);
+            Assert.AreEqual(expectedResponse.ProviderFeedback.EmployerFeedback.TotalEmployerResponses, result.EmployerFeedback.TotalEmployerResponses);
+            Assert.AreEqual(expectedResponse.ProviderFeedback.EmployerFeedback.TotalFeedbackRating, result.EmployerFeedback.TotalFeedbackRating);
+            Assert.AreEqual(expectedResponse.ProviderFeedback.EmployerFeedback.FeedbackAttributes.Count, result.EmployerFeedback.FeedbackAttributes.Count);
+        }
+    }
+}
--- src/SFA.DAS.ProviderFeedback.Web.UnitTests/Controllers/FeedbackControllerTests.cs
+++ src/SFA.DAS.ProviderFeedback.Web.UnitTests/Controllers/FeedbackControllerTests.cs
@@ -1,25 +1,37 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.ProviderFeedback.Web.Controllers;
 using SFA.DAS.ProviderFeedback.Web.ViewModels;
+using SFA.DAS.ProviderFeedback.Domain.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SFA.DAS.ProviderFeedback.Application.Queries.GetProviderFeedback;
+using static SFA.DAS.ProviderFeedback.Domain.GetProviderFeedback.Feedback;
 
 namespace SFA.DAS.ProviderFeedbackWeb.UnitTests.Controllers
 {
     [TestFixture] 
     public class FeedbackControllerTests
     {
+        private Mock<IOptions<SFA.DAS.ProviderFeedback.Domain.Configuration.ProviderFeedbackWeb>> _configMock;
+
         [SetUp]
         public void SetUp()
         {
-
+            var config = new SFA.DAS.ProviderFeedback.Domain.Configuration.ProviderFeedbackWeb
+            {
+                ShowReviewNotice = true,
+                ReviewNoticeDate = "Any text goes here"
+            };
+            _configMock = new Mock<IOptions<SFA.DAS.ProviderFeedback.Domain.Configuration.ProviderFeedbackWeb>>();
+            _configMock.Setup(ap => ap.Value).Returns(config);
         }
 
         [Test]
@@ -28,16 +40,35 @@ namespace SFA.DAS.ProviderFeedbackWeb.UnitTests.Controllers
             var mediatorMock = new Mock<IMediator>();
             var loggerMock = new Mock<ILogger<FeedbackController>>();
 
-            var controller = new FeedbackController(mediatorMock.Object, loggerMock.Object);
+            var providerId = 1234;
 
+            var queryResult = new GetProviderFeedbackResult 
+            { 
+                Ukprn = providerId, 
+                ApprenticeFeedback = new ApprenticeFeedback()
+                { 
+                    FeedbackAttributes = new List<ApprenticeFeedbackAttributeDetail>()
+                }, 
+                EmployerFeedback = new EmployerFeedback()
+                { 
+                    FeedbackAttributes = new List<EmployerFeedbackAttributeDetail>()
+                }
+            };
+            mediatorMock.Setup(x => x.Send(It.IsAny<GetProviderFeedbackQuery>(), default)).ReturnsAsync(queryResult);
+
+            var controller = new FeedbackController(mediatorMock.Object, loggerMock.Object, _configMock.Object);
+
+            
             var result =
-                await controller.Index(1234) as ViewResult;
+                await controller.Index(providerId) as ViewResult;
 
             Assert.That(result, Is.Not.Null);
             Assert.That(result.Model, Is.InstanceOf<ProviderFeedbackViewModel>());
             var viewModel = (ProviderFeedbackViewModel)result.Model;
             
-            //Assert.That(viewModel.ProviderId, Is.EqualTo(providerId));
+            Assert.That(viewModel.UKPRN, Is.EqualTo(providerId));
+            Assert.That(viewModel.ShowReviewNotice, Is.EqualTo(true));
+            Assert.That(viewModel.ReviewNoticeDate, Is.EqualTo(_configMock.Object.Value.ReviewNoticeDate));
         }
     }
 }

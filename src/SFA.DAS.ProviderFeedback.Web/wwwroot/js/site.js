891315e498e48b87691df4595c1a7542f7405c8f
David Rimar <david.rimar@digital.education.gov.uk>
2024-07-17 14:46:57 +0100

    EC-534 remove unused function

--- src/SFA.DAS.ProviderFeedback.Web/wwwroot/js/site.js
+++ src/SFA.DAS.ProviderFeedback.Web/wwwroot/js/site.js
@@ -10,49 +10,29 @@ function setCookie(name, value, days) {
     document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 
-// Function to read a cookie
-//function getCookie(name) {
-//    var nameEQ = name + "=";
-//    var ca = document.cookie.split(';');
-//    for (var i = 0; i < ca.length; i++) {
-//        var c = ca[i];
-//        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-//        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-//    }
-//    return null;
-//}
-
-// Function to get cookie
+ // Function to read a cookie
 function getCookie(name) {
-    const value = `; ${document.cookie}`;
-
-    console.log("value: ", value);
-
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) {
-        return parts.pop().split(';').shift();
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
     }
+    return null;
 }
-
-// write a function to get environment from host
-//function getEnvFromHost() {
-//    var host = window.location.host;
-//    console.log(host);
-
 
 //function get env
 function getEnvFromHost() {
     var host = window.location.host;
     console.log(host);
-
     var env = "";
 
     if (host.includes("at-pas")) {
         env = "AT";
     } else if (host.includes("test-pas")) {
         env = "TEST";
-    }
-    else if (host.includes("test2-pas")) {
+    } else if (host.includes("test2-pas")) {
         env = "TEST2";
     } else if (host.includes("pp-pas")) {
         env = "PP";
